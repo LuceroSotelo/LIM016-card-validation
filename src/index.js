2dd2980d08bae7ce6fce444d24724e89ec3b2a4a
LuceroSotelo <lucero.eds@gmail.com>
2021-11-09 09:48:24 -0500

    cambios presentación final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,93 @@
 import validator from './validator.js';
 
-console.log(validator);
+const formPago = document.getElementById("form")
+const numeroTarjeta = document.getElementById("numeroTarjeta")
+let numeroPrueba;
+const nombreTarjeta = document.getElementById("nombreTarjeta")
+const codigoDeSeguridad = document.getElementById("codigoDeSeguridad")
+const parrafo = document.getElementById("warnings")
+const modalContainer = document.getElementById("modalContainer")
+const cerrar = document.getElementById("cerrar")
+
+//Eliminando espacios en input numeroTarjeta
+numeroTarjeta.addEventListener("keyup", (event) => {
+    let valorInput = event.target.value;
+    numeroTarjeta.value = valorInput
+        //eliminando espacios
+        .replace(/\s/g, "")
+    //Eliminando valores que no sean números del 0 al 9
+    //.replace(/\D/g, "")
+});
+
+codigoDeSeguridad.addEventListener("keyup", (event) => {
+    let valorInput = event.target.value;
+    codigoDeSeguridad.value = valorInput
+        //eliminando espacios
+        .replace(/\s/g, "")
+        //Eliminando valores que no sean números del 0 al 9
+        .replace(/\D/g, "")
+});
+
+//funcion para obtener el valor del input numeroTarjeta y validarlo según el algoritmo de Luhn
+function validacionLuhn(event) {
+    event.preventDefault();
+    const nTarjetaValidado = validator.isValid(numeroPrueba);
+    console.log(nTarjetaValidado)
+}
+formPago.addEventListener("submit", validacionLuhn);
+
+//funcion para enmascarar numeroTarjeta.value al hacer "click" en input "nombre tarjeta"
+const mostrarEnmascarado = document.getElementById("nombreTarjeta");
+mostrarEnmascarado.addEventListener("click", enmascararNumeroTarjeta);
+
+
+function enmascararNumeroTarjeta(event) {
+    event.preventDefault();
+    let nTarjetaEnmascarada = (validator.maskify(numeroTarjeta.value));
+    numeroPrueba = numeroTarjeta.value;
+    numeroTarjeta.value = nTarjetaEnmascarada,
+        console.log(nTarjetaEnmascarada)
+}
+
+//Validación del formulario
+formPago.addEventListener("submit", e => {
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    parrafo.classList.remove("hide");
+    parrafo.innerHTML = ""
+
+    if (validator.isValid(numeroPrueba) === false) {
+        warnings += `El número de la tarjeta no es válido <br>`
+        entrar = true
+    }
+    if (nombreTarjeta.value.length < 3) {
+        warnings += `Ingresar Nombre y Apellido <br>`
+        entrar = true
+    }
+    if (codigoDeSeguridad.value.length < 3) {
+        warnings += `Ingresar código de seguridad <br>`
+        entrar = true
+    }
+    if (entrar) {
+        parrafo.innerHTML = warnings
+    }
+    else {
+        parrafo.innerHTML = ("Datos válidos");
+        //Mostrando ventana popup
+        modalContainer.classList.add("show")
+    }
+})
+
+//Cerrar ventana popup
+cerrar.addEventListener("click", () => {
+    modalContainer.classList.remove("show");
+    formPago.reset();
+    parrafo.classList.add("hide")
+});
+
+
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,46 @@
 const validator = {
-  // ...
-};
 
+  isValid: function (numero) {
+    console.log("este es el número: " + numero)
+    //convirtiendo input en un array
+    let arrayGuardar = numero.split("");
+    //inviertiendo el array
+    let arrayInvertido = arrayGuardar.reverse("");
+    //capturando elementos con indices inpares 
+    for (let i = 0; i < arrayInvertido.length; i++) {
+      if (i % 2 !== 0) {
+        arrayInvertido[i] = arrayInvertido[i] * 2;
+        if (arrayInvertido[i] >= 10) {
+          arrayInvertido[i] = arrayInvertido[i].toString().split("").map(Number);
+          arrayInvertido[i] = arrayInvertido[i].reduce((a, b) => {
+            return a + b;
+          });
+        }
+      }
+    }
+    arrayInvertido = arrayInvertido.map(Number).reduce((a, b) => {
+      return a + b;
+    });
+    console.log("Resultado de Luhn es:", arrayInvertido)
+    if (arrayInvertido % 10 === 0) {
+      console.log("Tarjeta válida")
+      return true
+    }
+    else {
+      console.log("Tarjeta inválida")   
+      return false        
+    }
+  },
+
+  maskify: function (numero) {
+    const enmascararN = (numero) => {
+      return numero.slice(0, -4).replace(/[0-9||a-z]/g, "#").concat(numero.slice(-4, numero.length));
+    }
+    return enmascararN(numero)
+  }
+};
 export default validator;
+
+
+
+
